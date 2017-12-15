b68cadefb3cb349b57df8c87dbdc9b5ca07808ea
KhanhNhat <hoangphamkhanhnhat@gmail.com>
2017-12-16 10:06:16 +1100

    Add a script for this project tasks.

---
+++ task_script.R
@@ -0,0 +1,37 @@
+#Task 1: Load package and data
+library(tidyverse)
+
+yearly = read_csv('yearly_deaths_by_clinic.csv')
+
+#Task 2: Create a new column: proportion_deaths
+yearly = mutate(yearly, proportion_deaths = deaths/births)
+
+#Taks 3: Plot proportion_deaths by year
+ggplot(yearly, aes(x = year, y = proportion_deaths, col = clinic)) +
+  geom_line()
+
+#Taks 4: Import monthly_deaths.csv
+monthly = read_csv('monthly_deaths.csv')
+monthly = mutate(monthly, proportion_deaths = deaths/births)
+head(monthly)
+
+#Taks 5: Plot proportion_death by month
+ggplot(monthly, aes(x = date, y = proportion_deaths)) +
+  geom_line()
+
+#Taks 6: Explore the effect of handwashing
+handwashing_start = as.Date('1847-06-01')
+monthly = mutate(monthly, handwashing_started = date >= handwashing_start)
+ggplot(monthly, aes(x = date, y = proportion_deaths, col = handwashing_started)) +
+  geom_line() + labs(x ='Date', y = 'Proportion of death: Before and After handwashing')
+
+#Task 7: Explore... continue
+summary = monthly %>%
+  group_by(handwashing_started) %>%
+  summarise(mean(proportion_deaths))
+
+summary
+
+#Task 8: Statistic analysis T-Test with montly dataset
+test_result = t.test(proportion_deaths ~ handwashing_started, data = monthly)
+test_result
